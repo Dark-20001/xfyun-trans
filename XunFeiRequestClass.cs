b9a325653c4577cad8372efed2985e2367a48c12
Dark-20001 <dark_2001@vista.aero>
2021-06-13 12:35:41 +0800

    Add files via upload

---
+++ XunFeiRequestClass.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITest
+{
+    public class XunFeiRequestClass
+    {
+        [JsonProperty("common")]
+        public common c { get; set; }
+        [JsonProperty("business")]
+        public business b { get; set; }
+        [JsonProperty("data")]
+        public data d { get; set; }
+    }
+
+    public class common
+    {
+        [JsonProperty("app_id")]
+        public string app_id { get; set; }
+    }
+
+    public class business
+    {
+        [JsonProperty("from")]
+        public string from { get; set; }
+        [JsonProperty("to")]
+        public string to { get; set; }
+    }
+
+    public class data
+    {
+        [JsonProperty("text")]
+        public string text { get; set; }
+    }
+}
